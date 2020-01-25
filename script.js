3a6c7bd4c6bd574135eb66657ec333e481f52f52
zlatta <violetta@plotnikovs.com>
2020-01-25 20:26:04 +0400

    one

---
+++ script.js
@@ -0,0 +1,11 @@
+const  one = document.getElementById("1")
+console.log(one)
+
+const input= document.getElementById("input")
+
+
+one.onclick = () =>{
+    
+    console.log(1)
+    input.value=1
+}
