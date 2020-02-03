3b4632f919c2a83ea2d6bbe3affe63f9a0c7488c
zlatta <violetta@plotnikovs.com>
2020-02-03 19:07:44 +0400

    оброботали цифры

--- script.js
+++ script.js
@@ -9,3 +9,66 @@ one.onclick = () =>{
     console.log(1)
     input.value=1
 }
+
+const tou =document.getElementById("2")
+tou.onclick = () => {
+    input.value=2
+}
+
+const three =document.getElementById("3")
+three.onclick = () => {
+    input.value=3
+}
+
+   
+const four =document.getElementById("4")
+four.onclick = () => {
+   input.value=4
+}
+
+
+const five =document.getElementById("5")
+five.onclick = () => {
+     input.value=5
+}
+
+
+const six =document.getElementById("6")
+six.onclick = () => {
+    console.log(454);
+    
+ input.value=6
+}
+
+
+const seven =document.getElementById("7")
+seven.onclick = () => {
+    input.value=7
+}
+
+
+const eight=document.getElementById("8")
+eight.onclick = () => {
+    input.value=8
+}            
+
+
+const nine=document.getElementById("9")
+nine.onclick = () => {
+   input.value=9
+}
+    
+
+
+
+const zero=document.getElementById("0")
+zero.onclick = () => {
+
+
+   input.value=0
+}
+    
+
+
+
+
